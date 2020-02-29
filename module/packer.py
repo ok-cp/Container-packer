18b634ad41539eb925b78176ea6b877072a147f6
ok-cp <bamdal0615@gmail.com>
2020-02-29 23:48:29 +0900

    v1

--- module/packer.py
+++ module/packer.py
@@ -9,5 +9,29 @@ print(sys.path)
 
 #import module.packer
 
+name = ['cat', 'dog', 'rat']
+
+try:
+    z = 'cat'
+#    z = 'bkg'
+    x = name.index(z)
+    print('{} Found it ! {} is '.format(z, x+1))
+
+#except ValueError as e:
+except ValueError as e:
+    print(e)
+    print('Not Fount. ValueError')
+else:
+    print('Ok! else')
+finally:
+    print('Ok! Finally')
+
+#print(name.index('cat'))
+print(name[0])
+
+
+
+
+
 if __name__ == "__main__":
     print("test")
